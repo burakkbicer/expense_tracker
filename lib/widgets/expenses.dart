7274d109c0cfc7aaec9a36462d6d46570ac7118d
Burak Bicer <burakbicer1616@gmail.com>
2023-07-03 16:59:46 +0300

    feat: Implemented expense deletion functionality through swipe gesture

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -47,6 +47,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,7 +69,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('Grafikler'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
