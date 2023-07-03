d0bfaaa8c74448d939ccbc83b3c873d00735e4b2
Burak Bicer <burakbicer1616@gmail.com>
2023-07-03 17:08:21 +0300

    feat: Added functionality to restore deleted expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -48,13 +48,40 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${expense.title} silindi.'),
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+          label: 'Geri Al',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('Kayıtlı harcamanız bulunmamaktadır.'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter GiderTakip'),
@@ -69,10 +96,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('Grafikler'),
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: mainContent,
           ),
         ],
       ),
